0dce7307713af9376367a18c6eebf444249f1362
Kitorishi <aealvarado07@gmail.com>
2026-03-03 19:53:03 -0800

    Cambios en logic.py para manejar la logica fuera de app

---
+++ backend/logic.py
@@ -0,0 +1,13 @@
+#Archivo donde vendra la Logica del programa
+#===========FUNCIONES Y CLASES============
+
+import random 
+
+def generar_tablero(n: int):
+    """Crear una matriz nxn con los numeros mezclados y un 0 (espacio vacio)."""
+
+def encontrar_posicion(tablero, valor):
+    """Busca en que fila y columna esta en un numero especifico."""
+
+def realizar_movimiento(tablero, pieza_a_mover):
+    """Revisa si la pieza se puede mover al espacio vacio (0) y luego intercambiamos."""
